4f55006f6c6f82712c80489b0cc1cdae109ee43f
omar taouil <70657213+liltwl@users.noreply.github.com>
2021-11-05 20:33:03 +0100

    Add files via upload

--- mimi/noobnoob/pipe.c
+++ mimi/noobnoob/pipe.c
@@ -6,7 +6,7 @@
 /*   By: otaouil <otaouil@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 19:44:48 by otaouil           #+#    #+#             */
-/*   Updated: 2021/11/05 17:00:48 by otaouil          ###   ########.fr       */
+/*   Updated: 2021/11/05 20:28:40 by otaouil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,15 +24,11 @@ void	exec_cmd(char **cmd1, t_data *l, t_cmd *cmd)
 	{
 		waitpid(pid, &status, 0);
 		l->exitstatu = WEXITSTATUS(status);
-		kill(pid, SIGTERM);
 	}
 	else
 	{
-		if (l->numcmd == 1)
-		{
-			dup2(cmd->in, 0);
-			dup2(cmd->out, 1);
-		}
+		dup2(cmd->in, 0);
+		dup2(cmd->out, 1);
 		execve(cmd->cmd, cmd1, NULL);
 		ft_putstr_fd("yoo: command not found\n", 2);
 		exit(127);
@@ -61,21 +57,17 @@ void	ft_check(t_data *l, t_cmd *cmd)
 		do_export(l, cmd);
 	else if (!strncmp(cmd->str[0], "unset", 6))
 		do_unset(l, cmd);
-	else
+	else if (l->numcmd == 1)
 		exec_cmd(cmd->str, l, cmd);
+	else
+		execve(cmd->cmd, cmd->str, NULL);
 }
 
 void	execdup(t_data *data, int *fds, int x, int fd)
 {
 	t_cmd	*cmd;
 	t_list	*tmp;
-	int		i;
 
-	i = -1;
-	tmp = data->cmd_list;
-	while (++i < x)
-		tmp = tmp->next;
-	cmd = tmp->content;
 	if (x != 0)
 	{
 		dup2(fd, 0);
@@ -83,97 +75,46 @@ void	execdup(t_data *data, int *fds, int x, int fd)
 	}
 	if (data->numcmd - 1 != x)
 		dup2(fds[1], 1);
+	close(fds[1]);
 	close(fds[0]);
 }
 
 void	ft_exitstatu(int *fd, int *fds, t_data *data, pid_t pid)
 {
-	wait(NULL);
+	if (*fd)
+		close(*fd);
 	close(fds[1]);
 	*fd = fds[0];
 }
 
 void	mlpipe(t_data *data)
 {
-	int		fds[g_data->numcmd * 2];
-	pid_t	pid;
-	int		x;
-	int		s;
-	int		j;
-
-	for( x = 0; x < g_data->numcmd; x++ )
-	{
-    	if( pipe(fds + x*2) < 0 )
-		{
-        	perror("pipe");
-    	}
-	}
-	j = 0;
-	x = -1;
-	while( ++x < g_data->numcmd )
-	{
-		pid = fork();
-		if( pid == 0 )
-		{
-			printf("%d -- %d\n",j - 2, j + 1);
-			if( x != 0 )
-			{
-				if( dup2(fds[j - 2], 0) < 0 )
-				{
-					perror("dup");
-				}
-			}
-			if( g_data->numcmd - 1 != x)
-			{
-				if( dup2(fds[j + 1], 1) < 0 )
-				{
-					perror("dup");
-				}
-			}
-			for( s = 0; s < 2 * g_data->numcmd; s++ )
-			{
-				close( fds[s] );
-			}
-			
-			ft_check(data, ft_findcmd(data->cmd_list, x));
-			exit(0);
-		}
-		else if( pid < 0 )
-		{
-			perror("fork");
-		}
-		j += 2;
-	}
-
-/* parent closes all of its copies at the end */
-for( x = 0; x < 2 * g_data->numcmd; x++ ){
-    close( fds[x] );
-}
-}
-
-void	mlpcipe(t_data *data)
-{
 	int		fds[2];
-	pid_t	pid;
+	pid_t	pid[data->numcmd];
 	int		i;
 	int		fd;
 
 	i = -1;
+	fd = 0;
 	while (++i < data->numcmd)
 	{
 		pipe(fds);
-		pid = fork();
-		if (pid == -1)
+		pid[i] = fork();
+		if (pid[i] == -1)
 			ft_putstr_fd("error : fork failed\n", 2);
-		else if (pid == 0)
+		else if (pid[i] == 0)
 		{
 			execdup(data, fds, i, fd);
 			ft_check(data, ft_findcmd(data->cmd_list, i));
-			exit(data->exitstatu);
+			exit(127);
 		}
 		else
-			ft_exitstatu(&fd, fds, data, pid);
+			ft_exitstatu(&fd, fds, data, pid[i]);
 	}
-	waitpid(pid, &data->exitstatu, 0);
+	close (fd);
+	i = -1;
+	while (++i < data->numcmd)
+		wait(&data->exitstatu);
+		//waitpid(pid[i], &data->exitstatu, 0);
 	data->exitstatu = WEXITSTATUS(data->exitstatu);
 }

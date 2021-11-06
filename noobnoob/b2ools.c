35b46302f557b7dd604482a96bda9148f4804805
omar taouil <70657213+liltwl@users.noreply.github.com>
2021-11-06 19:13:53 +0100

    Add files via upload

--- noobnoob/b2ools.c
+++ noobnoob/b2ools.c
@@ -6,7 +6,7 @@
 /*   By: otaouil <otaouil@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 19:56:06 by otaouil           #+#    #+#             */
-/*   Updated: 2021/11/06 16:12:00 by otaouil          ###   ########.fr       */
+/*   Updated: 2021/11/06 19:10:03 by otaouil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,14 @@ void	sig_handler(int sig)
 {
 	char p;
 
-	p = 3;
+	p = 10;
 	if (sig == SIGINT)
 	{
 		if(g_data->line)
 			free(g_data->line);
-		//write(1, "\n", 1);
+		rl_on_new_line();
+        rl_replace_line("", 0);
+        rl_redisplay();
 		ft_putchar_fd('\n', 1);
 		ft_putstr_fd("yoo$> ", 1);
 		//signal(SIGINT, sig_handler);
